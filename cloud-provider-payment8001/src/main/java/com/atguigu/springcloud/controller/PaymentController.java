0fc22c6558ab21e00f20985bded97ddd2adb497c
pineappleboss <374725192@qq.com>
2021-03-02 22:41:55 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    PaymentService paymentService;
+    @PostMapping("/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        if(result > 0){
+            return new CommonResult(200 , "数据插入成功" ,result);
+
+        }else{
+            return new CommonResult(200 , "数据插入失败" ,result);
+        }
+
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment result = paymentService.getPaymentById(id);
+        return new CommonResult(200,"",result);
+    }
+}

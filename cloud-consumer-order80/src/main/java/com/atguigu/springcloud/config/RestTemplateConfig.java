83da49eae6429ad6688c5627a127d1fb65f0dd3f
tiankaiqiang <374725192@qq.com>
2021-03-09 18:18:22 +0800

    s

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/RestTemplateConfig.java
@@ -8,7 +8,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+<<<<<<< Updated upstream
     @LoadBalanced
+=======
+    @LoadBalanced      //赋予 restTemplate负载均衡能力
+>>>>>>> Stashed changes
     public RestTemplate get(){
         return new RestTemplate();
     }
